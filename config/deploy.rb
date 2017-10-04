58c75836a54e5b126025a0999d444569f5d500fb
freedomsky <hanwei_1989@126.com>
2017-10-04 14:54:05 +0800

    Install capistrano-passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,10 +22,16 @@ set :deploy_to, "/home/apps/blog"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
+
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
+
+set :passenger_restart_with_touch, true
+
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
